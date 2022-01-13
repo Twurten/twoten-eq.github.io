1851328e33f7feaed6f265c5253a844da4a9a27e
2(10 <71557590+Twoten-equals@users.noreply.github.com>
2022-01-14 00:51:49 +0300

    jerry

---
+++ scripts/gridGen.js
@@ -0,0 +1,19 @@
+function generate(cwidth, cheight, rows, collumns) {
+    let canvas = document.getElementById("canvas");
+    console.log(canvas)
+    let ctx = canvas.getContext("2d");
+    canvas.width=cwidth;
+    canvas.height=cheight;
+    ctx.fillStyle="white"
+    ctx.fillRect(0,0,cwidth,cheight);
+    ctx.fillStyle="black"
+    for(let i = 0; i<collumns; i++){
+        for(let j = 0; j<rows; j++){
+            ctx.beginPath();
+            ctx.rect(i*cwidth/collumns,j*cheight/rows,cwidth/collumns,cheight/rows)
+            ctx.stroke()
+        }
+    }
+
+    
+}
--- scripts/skyblock.js
+++ scripts/skyblock.js
@@ -1,19 +1,21 @@
-let UUID = "469962ac63cd4792bd013da195c1a887";
-let API_KEY = "ae5b95fd-8a90-4466-9d75-f51d91ab475a";
+const API_KEY = "ae5b95fd-8a90-4466-9d75-f51d91ab475a";
+let ACCOUNT_UUID;
+let ACCOUNT;
 let profile;
 
-function sendMojangAPIRequest(username){
+function sendNOTMojangAPIRequest(username){
+    //MOjang api is fucking broken so yes
     if(username=="") return
     let request = new XMLHttpRequest()
     request.onreadystatechange = function(){
         if(this.readyState ==4 && this.status==200){
-            console.log(request)
-        }else{
-            console.log("ERR")
-        }
+            ACCOUNT = JSON.parse(request.response)
+            ACCOUNT_UUID = ACCOUNT.uuid
+            sendHypixelAPIRequest()
+        }else{}
     }
 
-    request.open("GET","https://api.mojang.com/users/profiles/minecraft/"+username)
+    request.open("GET","https://api.ashcon.app/mojang/v2/user/"+username)
     request.send()
 }
 
@@ -22,12 +24,12 @@ function sendHypixelAPIRequest(){
     let request = new XMLHttpRequest()
     request.onreadystatechange = function(){
         if(this.readyState ==4 && this.status==200){
-            profile = request.response;
+            profile = JSON.parse(request.response)
             console.log(profile)
         }
     }
 
-    request.open("GET","https://api.hypixel.net/skyblock/profiles?key="+API_KEY+"&uuid="+UUID)
+    request.open("GET","https://api.hypixel.net/skyblock/profiles?key="+API_KEY+"&uuid="+ACCOUNT_UUID)
     request.send()
 }
 
