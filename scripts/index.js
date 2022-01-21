bd374321b9936c82f8b5c9cb4d690ff4804c9ac4
2(10 <71557590+twoten-eq@users.noreply.github.com>
2022-01-21 13:31:09 +0300

    jerry!

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-function redirect(href, timeout){
-    let loc = location.toString().split('/');
-    let newloc = "";
-    for(let i = 0; i<loc.length-1; i++){
-        newloc+=loc[i]+"/";
-    }
-    setTimeout(function(){
-        location = newloc+href/*+".html"*/;
-    },timeout);
+function redirect(href, timeout) {
+  let loc = location.toString().split("/");
+  let newloc = "";
+  for (let i = 0; i < loc.length - 1; i++) {
+    newloc += loc[i] + "/";
+  }
+  setTimeout(function () {
+    location = newloc + href /*+".html"*/;
+  }, timeout);
 }
 
-function seticon(href){
-    document.head.querySelector("link").href=href;
+function seticon(href) {
+  document.head.querySelector("link").href = href;
 }
--- scripts/life.js
+++ scripts/life.js
@@ -1,125 +1,145 @@
-let canvas
-let ctx
-let generation
-let w, h
-let paused = false
+let canvas;
+let ctx;
+let generation;
+let w, h;
+let paused = false;
+let speedt = 1;
 
-function pause(){
-    paused=!paused
-    if(paused){
-        document.getElementById("pause").style="transform: rotate(90deg); clip-path: polygon(50% 0%, 0% 100%, 100% 100%);" 
-    }else{
-        document.getElementById("pause").style="transform: rotate(0deg); clip-path: none"
-    }
+function pause() {
+  paused = !paused;
+  if (paused) {
+    document.getElementById("pause").style =
+      "transform: rotate(90deg); clip-path: polygon(50% 0%, 0% 100%, 100% 100%);";
+  } else {
+    document.getElementById("pause").style =
+      "transform: rotate(0deg); clip-path: none";
+  }
 }
 
-function init(){
-    cs=10
-    w=200
-    h=200
-    generation = new Array(w)
-    for(let i = 0; i<generation.length; i++){
-        generation[i] = new Array(h)
-    }
-    fill(generation, "rnd")
-    canvas=document.getElementById("life")
-    canvas.addEventListener("click", setCell(), false)
-    ctx = canvas.getContext("2d")
-    canvas.width=w*cs
-    canvas.height=h*cs
-   
-    repaint()
-    window.requestAnimationFrame(step);
+function changespeed() {
+  speedt = (speedt + 1) % 3;
+  switch(speedt){
+    case 1:
+      document.getElementById("speed").firstElementChild.innerHTML="X1"
+      speed=10
+      break;
+    case 2:
+      document.getElementById("speed").firstElementChild.innerHTML="X2"
+      speed=5
+      break;
+    case 0:
+      document.getElementById("speed").firstElementChild.innerHTML="X.5"
+      speed=20
+      break;
+  }
+ 
 }
 
-function setCell(){
+function init() {
+  cs = 10;
+  w = 200;
+  h = 200;
+  generation = new Array(w);
+  for (let i = 0; i < generation.length; i++) {
+    generation[i] = new Array(h);
+  }
+  fill(generation, "rnd");
+  canvas = document.getElementById("life");
+  canvas.addEventListener("click", setCell(), false);
+  ctx = canvas.getContext("2d");
+  canvas.width = w * cs;
+  canvas.height = h * cs;
 
+  repaint();
+  window.requestAnimationFrame(step);
 }
+
+function setCell() {}
 
 let fill = (array, what) => {
-    if(what==0){
-        for(let i = 0; i<array.length; i++){
-            for(let j = 0; j<array[0].length; j++){
-                array[i][j] = 0
-            }
-        }
+  if (what == 0) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[0].length; j++) {
+        array[i][j] = 0;
+      }
     }
-    if(what=="rnd"){
-        for(let i = 0; i<array.length; i++){
-            for(let j = 0; j<array[0].length; j++){
-                array[i][j] = Math.round(Math.random())
-            }
-        }
+  }
+  if (what == "rnd") {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[0].length; j++) {
+        array[i][j] = Math.round(Math.random());
+      }
     }
-    if(what==1){
-        for(let i = 0; i<array.length; i++){
-            for(let j = 0; j<array[0].length; j++){
-                array[i][j] = 1
-            }
-        }
+  }
+  if (what == 1) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[0].length; j++) {
+        array[i][j] = 1;
+      }
     }
+  }
 
-    return array;
+  return array;
+};
+
+function repaint() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w * 10, h * 10);
+  ctx.fillStyle = "black";
+  for (let i = 0; i < generation.length; i++) {
+    for (let j = 0; j < generation[0].length; j++) {
+      if (generation[i][j] == 1) ctx.fillRect(i * 10, j * 10, cs, cs);
+    }
+  }
 }
 
-function repaint(){
-    ctx.fillStyle = "white"
-    ctx.fillRect(0,0,w*10,h*10)
-    ctx.fillStyle = "black"
-    for(let i = 0; i<generation.length; i++){
-        for(let j = 0; j<generation[0].length; j++){
-            if(generation[i][j]==1)
-                ctx.fillRect(i*10,j*10,cs,cs)
+function nextstep() {
+  let array = new Array(w);
+  for (let i = 0; i < array.length; i++) {
+    array[i] = new Array(h);
+  }
+
+  for (let ic = 0; ic < generation.length; ic++) {
+    for (let jc = 0; jc < generation[0].length; jc++) {
+      let aliveCells = 0;
+
+      for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+          if (!(i == 0 && j == 0)) {
+            if (generation[(ic + i + w) % w][(jc + j + h) % h] == 1)
+              aliveCells++;
+          }
         }
-    }
-}
+      }
 
-function nextstep(){
-    let array = new Array(w)
-    for(let i = 0; i<array.length; i++){
-        array[i] = new Array(h)
-    }
-
-    for(let ic = 0; ic<generation.length; ic++){
-        for(let jc = 0; jc<generation[0].length; jc++){
-            let aliveCells = 0;
-            
-            for(let i = -1; i<2; i++){
-                for(let j = -1; j<2; j++){
-                    if(!(i==0&&j==0)){
-                        if(generation[(ic+i+w)%w][(jc+j+h)%h]==1)
-                        aliveCells++
-                    }
-                }
-            }
-
-            if(generation[ic][jc]==1){
-                if(aliveCells==2||aliveCells==3){
-                    array[ic][jc]=1
-                }else{
-                    array[ic][jc]=0
-                }
-            }else{
-                if(aliveCells==3){
-                    array[ic][jc]=1
-                }
-            }
-            
+      if (generation[ic][jc] == 1) {
+        if (aliveCells == 2 || aliveCells == 3) {
+          array[ic][jc] = 1;
+        } else {
+          array[ic][jc] = 0;
         }
+      } else {
+        if (aliveCells == 3) {
+          array[ic][jc] = 1;
+        }
+      }
     }
-    generation=array
+  }
+  generation = array;
 }
 let stepc = 0;
 let speed = 10;
 
 function step(timestamp) {
-    if(!paused){
-        stepc++
-        if(stepc==speed){
-            nextstep()
-            repaint()
-            stepc=0
-        }
-    }   
-    window.requestAnimationFrame(step);
+  if (!paused) {
+    stepc++;
+    if (stepc == speed) {
+      nextstep();
+      repaint();
+      stepc = 0;
+    }
+  }
+
+  if(stepc>20) stepc=0
+  window.requestAnimationFrame(step);
 }
