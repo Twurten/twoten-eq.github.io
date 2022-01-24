dfda81274b827d30c4008e85d496442b20bed794
2(10 <71557590+twoten-eq@users.noreply.github.com>
2022-01-24 10:26:35 +0300

    jerry!

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,44 @@ function redirect(href, timeout) {
 function seticon(href) {
   document.head.querySelector("link").href = href;
 }
+let selected = null;
+function select(id){
+  if(selected!=null)
+    selected.classList.remove("selected")
+    document.getElementById(id).classList.add("selected")
+    selected = document.getElementById(id)
+}
+
+let selectedm = null;
+function selectm(id){
+  if(selectedm!=null)
+    selectedm.classList.remove("selected")
+    document.getElementById(id).classList.add("selected")
+    selectedm = document.getElementById(id)
+}
+
+function downloadpc(){
+  if(selected==null){
+    popup("Select an OS")
+    setTimeout(function(){popdown()}, 1000, false)
+  }
+}
+
+function downloadmb(){
+  if(selectedm==null){
+    popup("Select an OS")
+    setTimeout(function(){popdown()}, 1000, false)
+  }
+}
+
+function popup(txt){
+  let popup = document.getElementById("popup");
+  popup.style.opacity=1
+  popup.style.bottom="4em"
+  popup.innerText=txt
+}
+function popdown(){
+  let popup = document.getElementById("popup");
+  popup.style.opacity=0
+  popup.style.bottom="-4em"
+}
