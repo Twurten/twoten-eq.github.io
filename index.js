f7a5d70e96f5c427804020f8018c31118915ceb8
2(10 <71557590+twoten-eq@users.noreply.github.com>
2022-04-15 10:11:28 +0300

    E

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function load(){
         }
         
         if(JSON.parse(request.responseText).length==0){
-            document.getElementById("loading").innerText=""
+            document.getElementById("loading").innerText="twoten-eq/"
             for(let i = 0; i<array.length; i++){
-                createRepoDiv(array[i].name, array[i].html_url, array[i].description)
+                createRepoDiv(array[i].name, array[i].html_url, array[i].description, array[i].stargazers_count)
             }
             
             return;
@@ -30,9 +30,19 @@ function load(){
     }
 }
 
-function createRepoDiv(title, link, description){
+function createRepoDiv(title, link, description, stars){
     let el = document.createElement("div")
     el.classList.add("repo")
+    if(stars!=0){
+        let star = document.createElement("div")
+        star.innerHTML='<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-star-fill starred-button-icon d-inline-block mr-2"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25z"></path></svg>'
+        star.classList.add("star")
+        let amount = document.createElement("div")
+        amount.innerText=stars
+        amount.classList.add("amount")
+        star.appendChild(amount)
+        el.appendChild(star)
+    }
     let h1 = document.createElement("a")
     h1.innerText=title
     h1.href=link;
